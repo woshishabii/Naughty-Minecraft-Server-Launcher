4befd73f537ea10a1f77b9cb58d24793779d6a69
星辰 <jgbsxx20130315@163.com>
2021-08-02 15:45:56 +0800

    修复了不能匹配部分文件大小的bug(feature)

--- getServerJars.py
+++ getServerJars.py
@@ -1,3 +1,5 @@
+# By. woshishabi
+
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,6 +8,7 @@ class LinkHandler():
     def __init__(self):
         self.source = {
             'vanilla':'https://getbukkit.org/download/vanilla',
+            'spigot':'https://getbukkit.org/download/spigot',
         }
         self.data = {}
         self.soups = {}
@@ -29,7 +32,7 @@ class LinkHandler():
         for temp in self.soups['vanilla'].find_all('h3'):
             temp = str(temp)[4:-5]
             # print(temp)
-            if re.match(r'^[0-9][0-9].[0-9] MB$', temp, re.I|re.M):
+            if re.match(r'^[0-9][0-9].[0-9] MB$', temp, re.I|re.M) or re.match(r'^[0-9].[0-9] MB$', temp, re.I|re.M) or re.match(r'^[0-9][0-9].[0-9][0-9] MB$', temp, re.I|re.M) or re.match(r'^[0-9].[0-9][0-9] MB$', temp, re.I|re.M):
                 self.sizes['vanilla'].append(temp)
             else:
                 for weekday in self.weekdays:
@@ -40,6 +43,38 @@ class LinkHandler():
             if temp.get('href').startswith('https://getbukkit.org/get/'):
                 self.links['vanilla'].append(temp.get('href'))
 
+    def resolveSpigotLinks(self):
+        # Get data of the getbukkit website and instance it into a BeautifulSoup object
+        self.data['spigot'] = requests.get(self.source['spigot'])
+        self.soups['spigot'] = BeautifulSoup(self.data['spigot'].text, 'html.parser')
+        # Get version list
+        self.versions['spigot'] = []
+        for temp in self.soups['spigot'].find_all('h2'):
+            self.versions['spigot'].append(str(temp)[4:-5])
+        self.sizes['spigot'] = []
+        self.release_date['spigot'] = []
+        for temp in self.soups['spigot'].find_all('h3'):
+            temp = str(temp)[4:-5]
+            # print(temp)
+            if re.match(r'^[0-9][0-9].[0-9] MB$', temp, re.I|re.M) or re.match(r'^[0-9][0-9].[0-9][0-9] MB$', temp, re.I|re.M) or re.match(r'^[0-9][0-9].[0-9][0-9]$'):
+                self.sizes['spigot'].append(temp)
+            else:
+                for weekday in self.weekdays:
+                    if weekday in temp:
+                        self.release_date['spigot'].append(temp)
+        self.links['spigot'] = []
+        for temp in self.soups['spigot'].find_all('a'):
+            if temp.get('href').startswith('https://getbukkit.org/get/'):
+                self.links['spigot'].append(temp.get('href'))
+
+test = LinkHandler()
+test.resolveVanillaLinks()
+
+# print(test.versions)
+print(test.sizes)
+# print(test.links)
+
+''' 测试程序
 test = LinkHandler()
 test.resolveVanillaLinks()
 
@@ -54,3 +89,4 @@ with open('release_date.json', mode='w') as f:
 
 with open('links.json', mode='w') as f:
     f.write(str(test.links))
+'''
